cf32c99988a71be50009e13c911cedcdf195b3c7
MMallea <40362079+MMallea@users.noreply.github.com>
2023-07-08 13:47:49 -0400

    Kenney block?

---
+++ GMTK2023/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixerGroup mainMixer;
+    public Sound[] music;
+    public Sound[] sounds;
+    public Sound curTrack;
+    public string startTrack;
+
+    private static AudioManager instance;
+
+    public static AudioManager Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one AudioManager in the scene!");
+        }
+        else
+        {
+            instance = this;
+        }
+
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.outputAudioMixerGroup = mainMixer;
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+        foreach (Sound m in music)
+        {
+            m.source = gameObject.AddComponent<AudioSource>();
+            m.source.outputAudioMixerGroup = mainMixer;
+            m.source.clip = m.clip;
+
+            m.source.volume = m.volume;
+            m.source.pitch = m.pitch;
+            m.source.loop = m.loop;
+        }
+    }
+
+    public void Start()
+    {
+        curTrack = Array.Find(music, sound => sound.name == startTrack);
+        if (curTrack == null)
+        {
+            Debug.LogWarning("Music: " + name + " not found");
+            return;
+        }
+        curTrack.source.Play();
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void ChangeTrack(string name)
+    {
+        if (curTrack.name == name)
+        {
+            return;
+        }
+
+        curTrack.source.Stop();
+        curTrack = null;
+
+        curTrack = Array.Find(music, sound => sound.name == name);
+        if (curTrack == null)
+        {
+            Debug.LogWarning("Music: " + name + " not found");
+            return;
+        }
+        curTrack.source.Play();
+    }
+}
